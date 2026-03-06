3f5a6a6547adc5b9eaa3b9e565710f54b38fa405
EggsellentGuy <ivanrossi112@gmail.com>
2026-03-06 13:07:00 +0300

    add 2 steap to the main

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "django_bootstrap5",
     "task_manager",
 ]
 
@@ -125,4 +127,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
