2470e9fac2a17803da9954abe12b075fb5e10c23
EggsellentGuy <ivanrossi112@gmail.com>
2026-03-10 23:32:43 +0300

    Fix bug

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,9 +1,22 @@
 from django import forms
+from django.contrib.auth.models import User
 
 from tasks.models import Task
 
 
+class UserChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        full_name = obj.get_full_name()
+        return full_name if full_name else obj.username
+
+
 class TaskForm(forms.ModelForm):
+    executor = UserChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        label="Исполнитель",
+    )
+
     class Meta:
         model = Task
         fields = ("name", "description", "status", "executor", "labels")
