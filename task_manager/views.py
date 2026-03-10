9bec0453ad296a1bf1056f4e9c66b072b10c7e0e
EggsellentGuy <ivanrossi112@gmail.com>
2026-03-10 15:15:41 +0300

    Remove Rollbar test endpoint

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
     return render(request, "index.html")
-
-
-def test_error(request):
-    a = None
-    a.hello()
-    return HttpResponse("ok")
